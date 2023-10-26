5e11253d86efd5cb5d6fed4f37c941d8d44b04c2
Misak <azurovymisak@outlook.com>
2023-10-26 03:23:57 -0700

    Initial Commit

---
+++ PVMon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PVMon
+//
+//  Created by Sysprobs on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PVMon/PVMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PVMonApp.swift
+//  PVMon
+//
+//  Created by Sysprobs on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PVMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
